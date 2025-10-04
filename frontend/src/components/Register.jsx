cf336b3cb3464a0aaf40af569d9a7895a1593067
Rishima <rishima1711@gmail.com>
2025-10-04 23:14:20 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:3000/api/auth/register", {
+      const res = await axios.post("https://leftoverfood-donation.onrender.com/api/auth/register", {
         username,
         email,
         password,
